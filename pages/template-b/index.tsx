dc4d7d73af3719d3a8256b0300a73c2b3e176619
Ibrahim <ibrahimbmohammed@gmail.com>
2022-10-27 17:12:15 +0100

    add: first success message

---
+++ pages/template-b/index.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+function TemplateOne() {
+  const [recievedMessage, setRecievedMessage] = useState("");
+
+  const sendMessage = () => {
+    window.parent.postMessage("hi from child", "http://localhost:3001");
+  };
+  useEffect(() => {
+    window.addEventListener("message", function (e) {
+      if (e.origin !== "http://localhost:3001") return;
+      setRecievedMessage("got from parent" + e.data);
+    });
+  }, []);
+
+  return (
+    <div className="flex flex-col space-y-10">
+      <p className="text-lg">TemplateOne:{recievedMessage}</p>{" "}
+      <button onClick={() => sendMessage()}>send message to parent</button>
+    </div>
+  );
+}
+
+export default TemplateOne;
