1d609b5557533e5899ad7f90fead2353481c28b7
Ibrahim <ibrahimbmohammed@gmail.com>
2022-11-26 17:11:07 +0100

    add: push 3

---
+++ pages/template-c/index.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+function TemplateOne() {
+  const [recievedMessage, setRecievedMessage] = useState("");
+
+  const sendMessage = () => {
+    window.parent.postMessage("hi from child", "http://localhost:3001");
+  };
+  useEffect(() => {
+    console.log("hi in b");
+    window.addEventListener("message", function (e) {
+      setRecievedMessage("got from parent" + e.data);
+    });
+  }, []);
+
+  return (
+    <div className="flex flex-col space-y-10">
+      <p className="text-lg h-full w-full bg-pink-600">
+        TemplateB:{recievedMessage}
+      </p>{" "}
+      <button onClick={() => sendMessage()}>send message to parent</button>
+    </div>
+  );
+}
+
+export default TemplateOne;
